6115730c683cadc58603617daf52a27953286328
Mark Wilson <mark@warkmilson.com>
2016-10-28 17:12:02 -0700

    Show time at center of watch

--- pebble/src/main.c
+++ pebble/src/main.c
@@ -3,7 +3,8 @@
 #define LONG_CLICK_DELAY_MS 500
 
 static Window *s_window;
-static TextLayer *s_text_layer;
+static TextLayer *s_sound_text;
+static TextLayer *s_clock_text;
 static TextLayer *s_connected_text;
 
 static time_t last_vibe_start = 0;
@@ -13,7 +14,7 @@ static void send_message(const char * message) {
   last_vibe_start = time(NULL);
   vibes_short_pulse();
 
-  text_layer_set_text(s_text_layer, message);
+  text_layer_set_text(s_sound_text, message);
 
   DictionaryIterator *iter;
   app_message_outbox_begin(&iter);
@@ -73,7 +74,19 @@ static void in_received_handler(DictionaryIterator *received, void *context) {
 static void in_dropped_handler(AppMessageResult reason, void *context) {}
 
 static void out_failed_handler(DictionaryIterator *failed, AppMessageResult reason, void *context) {
-  text_layer_set_text(s_text_layer, "failed");
+  text_layer_set_text(s_sound_text, "failed");
+}
+
+static void minute_handler(struct tm *tick_time, TimeUnits units_changed) {
+  static char time_string[16];
+  clock_copy_time_string(time_string, 16);
+  // cut off " AM"/" PM"
+  if (time_string[4] == ' ') {
+    time_string[4] = 0;
+  } else {
+    time_string[5] = 0;
+  }
+  text_layer_set_text(s_clock_text, time_string);
 }
 
 static void window_load(Window *window) {
@@ -85,19 +98,28 @@ static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
-  s_text_layer = text_layer_create(GRect(0, 68, bounds.size.w, 32));
-  text_layer_set_font(s_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
-  text_layer_set_text_alignment(s_text_layer, GTextAlignmentCenter);
-  layer_add_child(window_layer, text_layer_get_layer(s_text_layer));
+  s_sound_text = text_layer_create(GRect(0, 10, bounds.size.w, 32));
+  text_layer_set_font(s_sound_text, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
+  text_layer_set_text_alignment(s_sound_text, GTextAlignmentCenter);
+  layer_add_child(window_layer, text_layer_get_layer(s_sound_text));
 
-  s_connected_text = text_layer_create(GRect(0, 130, bounds.size.w, 32));
+  s_clock_text = text_layer_create(GRect(0, bounds.size.h / 2 - 26, bounds.size.w, 50));
+  text_layer_set_font(s_clock_text, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  text_layer_set_text_alignment(s_clock_text, GTextAlignmentCenter);
+  layer_add_child(window_layer, text_layer_get_layer(s_clock_text));
+
+  s_connected_text = text_layer_create(GRect(0, bounds.size.h - 25, bounds.size.w, 32));
   text_layer_set_font(s_connected_text, fonts_get_system_font(FONT_KEY_GOTHIC_14));
   text_layer_set_text_alignment(s_connected_text, GTextAlignmentCenter);
   layer_add_child(window_layer, text_layer_get_layer(s_connected_text));
+
+  tick_timer_service_subscribe(MINUTE_UNIT, minute_handler);
 }
 
 static void window_unload(Window *window) {
-  text_layer_destroy(s_text_layer);
+  text_layer_destroy(s_sound_text);
+  text_layer_destroy(s_clock_text);
+  text_layer_destroy(s_connected_text);
 }
 
 static void init(void) {
