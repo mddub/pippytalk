90a4a4e7f7a6c3df5942e20b751b4861194f5dc9
Mark Wilson <mark@warkmilson.com>
2016-10-28 18:28:45 -0700

    Send command to play sound when Pebble message received

--- nRF8001-Swift/ViewController.swift
+++ nRF8001-Swift/ViewController.swift
@@ -7,14 +7,18 @@
 //
 
 import UIKit
+import PebbleKit
 
-class ViewController: UIViewController, NRFManagerDelegate {
+class ViewController: UIViewController, NRFManagerDelegate, PBPebbleCentralDelegate {
     
     var nrfManager:NRFManager!
     var feedbackView = UITextView()
 
-    var nextSoundIndex: Int = 0
-    
+    var pebbleCentral: PBPebbleCentral!
+    var activeWatch: PBWatch?
+
+    let pebbleAppUUID = UUID(uuidString: "a9931d36-1041-44a2-a8ba-e0e96cf114bc")!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -24,14 +28,16 @@ class ViewController: UIViewController, NRFManagerDelegate {
         nrfManager.delegate = self
         
         setupUI()
+
+        self.activeWatch = nil
+        pebbleCentral = PBPebbleCentral.default()
+        pebbleCentral.addAppUUID(pebbleAppUUID)
+        pebbleCentral.delegate = self
     }
-    
-    func sendData()
-    {
-        let string = "\(nextSoundIndex)"
-        let result = self.nrfManager.writeString(string)
-        log("↑ Sent string: \(string) - Result: \(result)")
-        nextSoundIndex = (nextSoundIndex + 1) % 9
+
+    func connectToWatch() {
+        log("Connecting to watch...")
+        pebbleCentral.run()
     }
 }
 
@@ -40,16 +46,67 @@ extension ViewController
 {
     func nrfDidConnect(_ nrfManager:NRFManager)
     {
-        self.log("★ Connected")
+        self.log("★ Connected to collar")
     }
     
     func nrfDidDisconnect(_ nrfManager:NRFManager)
     {
-        self.log("★ Disconnected")
+        self.log("★ Disconnected from collar")
     }
     
     func nrfReceivedData(_ nrfManager:NRFManager, data: Data?, string: String?) {
-        self.log("↓ Received data - String: \(string) - Data: \(data)")
+        self.log("↓ Received collar data - String: \(string) - Data: \(data)")
+    }
+}
+
+// MARK: - PBPebbleCentralDelegate methods
+extension ViewController {
+    @objc func pebbleCentral(_ central: PBPebbleCentral, watchDidConnect watch: PBWatch, isNew: Bool) {
+        log("watchDidConnect: \(watch.name)")
+        guard activeWatch == nil else {
+            return
+        }
+        activeWatch = watch
+
+        DispatchQueue.main.async {
+            watch.appMessagesAddReceiveUpdateHandler(self.handleAppMessage, with: self.pebbleAppUUID)
+            self.log("subscribed")
+        }
+
+        // The AppMessage channel must be opened by the phone
+        sendAppMessage(message: [:], withUUID: self.pebbleAppUUID)
+    }
+
+    @objc func pebbleCentral(_ central: PBPebbleCentral, watchDidDisconnect watch: PBWatch) {
+        log("watchDidDisconnect: \(watch.name)")
+        guard activeWatch == watch else {
+            return
+        }
+        activeWatch = nil
+    }
+
+    func sendAppMessage(message: [NSNumber : AnyObject], withUUID uuid: UUID) {
+        guard let watch = activeWatch else {
+            log("Can't send message: no Pebble watch connected")
+            return
+        }
+
+        log("sending \(message)")
+
+        DispatchQueue.main.async {
+            watch.appMessagesPushUpdate(message, with: uuid)
+        }
+    }
+
+    func handleAppMessage(watch: PBWatch, message: [NSNumber : Any]) -> Bool {
+        log("Received watch message: \(message)")
+
+        if let string = message[0] as? String {
+            let result = self.nrfManager.writeString(string)
+            self.log("↑ Sent string to collar: \(string) - Result: \(result)")
+        }
+
+        return true
     }
 }
 
@@ -64,28 +121,13 @@ extension ViewController {
         
         let connectButton:UIButton =  UIButton(type: .system)
         view.addSubview(connectButton)
-        connectButton.setTitle("Connect", for: UIControlState())
+        connectButton.setTitle("Connect to watch", for: UIControlState())
         connectButton.translatesAutoresizingMaskIntoConstraints = false
-        connectButton.addTarget(nrfManager, action: Selector(("connect")), for: UIControlEvents.touchUpInside)
-        
-        let disconnectButton:UIButton = UIButton(type: .system)
-        view.addSubview(disconnectButton)
-        disconnectButton.setTitle("Disconnect", for: UIControlState())
-        disconnectButton.translatesAutoresizingMaskIntoConstraints = false
-        disconnectButton.addTarget(nrfManager, action: Selector(("disconnect")), for: UIControlEvents.touchUpInside)
-        
-        let sendButton:UIButton = UIButton(type: .system)
-        view.addSubview(sendButton)
-        sendButton.setTitle("Send Data", for: UIControlState())
-        sendButton.translatesAutoresizingMaskIntoConstraints = false
-        sendButton.addTarget(self, action: #selector(ViewController.sendData), for: UIControlEvents.touchUpInside)
-        
-        
+        connectButton.addTarget(self, action: #selector(ViewController.connectToWatch), for: UIControlEvents.touchUpInside)
+
         view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-[f]-|", options: [], metrics: nil, views: ["f":feedbackView]))
         view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-[c]-|", options: [], metrics: nil, views: ["c":connectButton]))
-        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-[d]-|", options: [], metrics: nil, views: ["d":disconnectButton]))
-        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-[s]-|", options: [], metrics: nil, views: ["s":sendButton]))
-        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-20-[f]-[c]-[d]-[s]-20-|", options: [], metrics: nil, views: ["f":feedbackView,"c":connectButton,"d":disconnectButton,"s":sendButton]))
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-20-[f]-[c]-20-|", options: [], metrics: nil, views: ["f":feedbackView,"c":connectButton]))
     }
     
     func log(_ string:String)
